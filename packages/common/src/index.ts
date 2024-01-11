4aaa7f34f08d31db02fa4b987abde4e2275523f7
border-collie <colliedog1001@gmail.com>
2024-01-11 19:48:06 +0800

    feat: type atomic

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -3,6 +3,7 @@ import { type BaseConfig } from "@colliejs/core";
 import { defaultConfig } from "@colliejs/config";
 
 //必须要设置断点，否则dynmaic variants无法传入数组
+
 export const styled = makeStyled({
   ...defaultConfig,
   breakpoints: [0, 768],
