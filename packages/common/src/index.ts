c3369fa2e50fa568cda22b9e9bc2b6c1109a4b37
border-collie <colliedog1001@gmail.com>
2024-01-11 17:15:00 +0800

    feat: type fix

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,7 +1,10 @@
 import { makeStyled } from "@colliejs/react";
 import { type BaseConfig } from "@colliejs/core";
 import { defaultConfig } from "@colliejs/config";
+
+//必须要设置断点，否则dynmaic variants无法传入数组
 export const styled = makeStyled({
   ...defaultConfig,
+  breakpoints: [0, 768],
   prefix: "co",
 } as const satisfies BaseConfig);
